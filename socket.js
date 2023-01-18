484dad33421c392c3dba59806bdd9e14c3191e00
Sachin Raj <sachintk99@gmail.com>
2023-01-19 00:29:24 +0530

    commit after setting response headers

--- socket.js
+++ socket.js
@@ -5,8 +5,25 @@ const app = express()
 
 
 // app.options('*', cors());
-app.use(cors({origin: ['https://www.th.technophil.xyz', 'http://127.0.0.1:8800']}));
+// app.use(cors({origin: ['https://www.th.technophil.xyz', 'http://127.0.0.1:8800']}));
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.th.technophil.xyz');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 
 const io = require('socket.io')(8800)
